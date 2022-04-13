86f21137a9e59a99ebf78acc96c668ff8b01f4e3
kameron <kamscott3@gmail.com>
2022-04-12 20:17:42 -0400

    hw

---
+++ index.js
@@ -0,0 +1,36 @@
+var a = 10
+var b = 20
+
+
+function addSum(param1, param2) {
+    console.log(param1 + param2)
+}
+addSum(a,b);
+
+var c = 12
+var d = 4
+
+
+function difference(param1, param2){
+    console.log(param1 - param2)
+}
+difference(c,d);
+
+var a = 60
+var b = 10
+
+
+function product(param1, param2) {
+    console.log(param1 * param2)
+}
+product(a,b);
+
+
+var c = 16
+var d = 4
+
+
+function quotient(param1, param2) {
+    console.log(param1 / param2)
+}
+quotient(c,d);
